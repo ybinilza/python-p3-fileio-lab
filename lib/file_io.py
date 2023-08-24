4723e7e72ab262f8afd20a27b4d19c59a89f9bf5
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-08-24 12:28:14 -0400

    Lab Completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    file_name=str(file_name)+".txt";
+    with open(file_name,mode="w",encoding="utf-8") as file_name:
+        file_name.write(file_content);
+
 
 def append_file(file_name, append_content):
-    pass
+    file_name=str(file_name)+".txt";
+    with open(file_name,mode="a",encoding="utf-8") as file_name:
+        file_name.write(append_content);
 
 def read_file(file_name):
-    pass
+    file_name=str(file_name)+".txt";
+    with open(file_name, encoding='utf-8') as file_name:
+        contents=file_name.read()
+    return contents
